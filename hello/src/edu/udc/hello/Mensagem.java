38e4eb303f2ff0f3aa33b7061a029937f76f82dd
Alessandra Bussador <bussador@yahoo.com.br>
2019-03-31 21:49:57 -0300

    retirada mensagem

--- hello/src/edu/udc/hello/Mensagem.java
+++ hello/src/edu/udc/hello/Mensagem.java
@@ -3,7 +3,7 @@ package edu.udc.hello;
 public class Mensagem {
   public void mensagem()
   {
-	  System.out.println("classe mensagem");
+	  System.out.println("mensagem");
 	  System.out.println("classe mensagem2");
   }
 }
